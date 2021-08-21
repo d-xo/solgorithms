90e4ac4d5f1bbb63d34e9ee9fbc334f561bb3897
Leo Alt <leo@ethereum.org>
2021-08-21 10:46:40 +0200

    fix LSOne

--- src/FenwickTree.sol
+++ src/FenwickTree.sol
@@ -8,17 +8,20 @@ struct FenwickTree
 library FenwickTreeImpl
 {
 
-function create(FenwickTree memory t, uint n) pure internal {
+function create(FenwickTree memory t, uint n) pure internal
+{
 	t.data = new uint[](n + 1);
 }
 
 /// @return `n`'s least significat bit.
-function LSOne(uint n) pure internal returns (uint) {
-	return n & uint((-int(n)));
+function LSOne(uint n) pure internal returns (uint)
+{
+	return uint(int(n) & (-int(n)));
 }
 
 /// @return RQS(1, r).
-function rangeSumQuery(FenwickTree memory t, uint r) pure internal returns (uint) {
+function rangeSumQuery(FenwickTree memory t, uint r) pure internal returns (uint)
+{
 	uint sum = 0;
 	for(; r > 0; r -= LSOne(r))
 		sum += t.data[r];
@@ -26,11 +29,13 @@ function rangeSumQuery(FenwickTree memory t, uint r) pure internal returns (uint
 }
 
 /// @return RQS(l, r).
-function rangeSumQuery(FenwickTree memory t, uint l, uint r) pure internal returns (uint) {
+function rangeSumQuery(FenwickTree memory t, uint l, uint r) pure internal returns (uint)
+{
 	return rangeSumQuery(t, r) - (l == 1 ? 0 : rangeSumQuery(t, l - 1));
 }
 
-function update(FenwickTree memory t, uint k, uint x) pure internal {
+function update(FenwickTree memory t, uint k, uint x) pure internal
+{
 	for(; k < t.data.length; k += LSOne(k))
 		t.data[k] += x;
 }
