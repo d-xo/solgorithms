c7b9189b9280a73eae1368e3328a000357582834
David Terry <me@xwvvvvwx.com>
2021-08-23 12:43:21 +0200

    standardize lib definitions

--- src/FenwickTree.sol
+++ src/FenwickTree.sol
@@ -5,39 +5,38 @@ struct FenwickTree
 	uint[] data;
 }
 
-library FenwickTreeImpl
-{
+library FenwickTreeImpl {
 
-function create(FenwickTree memory t, uint n) pure internal
-{
-	t.data = new uint[](n + 1);
-}
+    function create(FenwickTree memory t, uint n) pure internal
+    {
+        t.data = new uint[](n + 1);
+    }
 
-/// @return `n`'s least significat bit.
-function LSOne(uint n) pure internal returns (uint)
-{
-	return uint(int(n) & (-int(n)));
-}
+    /// @return `n`'s least significat bit.
+    function LSOne(uint n) pure internal returns (uint)
+    {
+        return uint(int(n) & (-int(n)));
+    }
 
-/// @return RQS(1, r).
-function rangeSumQuery(FenwickTree memory t, uint r) pure internal returns (uint)
-{
-	uint sum = 0;
-	for(; r > 0; r -= LSOne(r))
-		sum += t.data[r];
-	return sum;
-}
+    /// @return RQS(1, r).
+    function rangeSumQuery(FenwickTree memory t, uint r) pure internal returns (uint)
+    {
+        uint sum = 0;
+        for(; r > 0; r -= LSOne(r))
+            sum += t.data[r];
+        return sum;
+    }
 
-/// @return RQS(l, r).
-function rangeSumQuery(FenwickTree memory t, uint l, uint r) pure internal returns (uint)
-{
-	return rangeSumQuery(t, r) - (l == 1 ? 0 : rangeSumQuery(t, l - 1));
-}
+    /// @return RQS(l, r).
+    function rangeSumQuery(FenwickTree memory t, uint l, uint r) pure internal returns (uint)
+    {
+        return rangeSumQuery(t, r) - (l == 1 ? 0 : rangeSumQuery(t, l - 1));
+    }
 
-function update(FenwickTree memory t, uint k, uint x) pure internal
-{
-	for(; k < t.data.length; k += LSOne(k))
-		t.data[k] += x;
-}
+    function update(FenwickTree memory t, uint k, uint x) pure internal
+    {
+        for(; k < t.data.length; k += LSOne(k))
+            t.data[k] += x;
+    }
 
 }
